fa26ae48c01680bcb2dd3939016510b0102b1513
Sven Schoradt <sven.schoradt@googlemail.com>
2025-02-05 22:28:12 +0100

    SYS basic form and style

--- src/app/time-dialog/time-dialog.component.ts
+++ src/app/time-dialog/time-dialog.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-time-dialog',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule],
   templateUrl: './time-dialog.component.html',
-  styleUrl: './time-dialog.component.css'
+  styleUrl: './time-dialog.component.scss'
 })
 export class TimeDialogComponent {}
